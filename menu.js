8e78cecca5482d8f0d5e6b880437f02bf7476a65
ModernTherapy <autobotclap@gmail.com>
2025-02-19 09:41:13 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("menu.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("menu-container").innerHTML = data;
+            document.body.insertAdjacentHTML("afterbegin", data);
         })
         .catch(error => console.error("Error loading menu:", error));
 });
